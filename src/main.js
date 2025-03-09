164154428697ecff2c79302e9dab06d6ccfbe94d
Vnz-007 <viniciusloiolajg@gmail.com>
2025-03-09 15:12:14 -0300

    test API Deploy

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@
 import "./libs/dayjs.js";
 
 // CSS
-import "src/styles/global.css";
-import "src/styles/form.css";
-import "src/styles/schedule.css";
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
 
 // JS
 import "./modules/form/submit.js";
