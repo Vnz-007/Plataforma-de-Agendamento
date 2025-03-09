108778773c73356bac62f20b125cb1a503f5bc38
Vnz-007 <viniciusloiolajg@gmail.com>
2025-03-09 13:30:29 -0300

    Projeto - 100%

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,20 +1,30 @@
+import { schedulesDay } from "./load.js";
+import { scheduleCancel } from "../../services/schedule-cancel.js";
+
 const periods = document.querySelectorAll(".period");
 
 // Gera evento click para cada lista (li = manhã, tarde e noite).
-periods.forEach((periods) => {
-  periods.addEventListener("click", (event) => {
+periods.forEach((period) => {
+  // Captura o evento de click na lista (li).
+  period.addEventListener("click", async (event) => {
     if (event.target.classList.contains("cancel-icon")) {
       // Obtém a li pai do elemento clicado.
-      const item = event.target.cosest("li");
+      const item = event.target.closest("li");
+
+      // Pega o id do agendamento para remover.
       const { id } = item.dataset;
 
+      // Confirma que o id foi selecionado.
       if (id) {
-        const isConfirm = confirm(
-          "Você realmente deseja cancelar o agendamento?"
-        );
+        // Confirma se o usuário quer cancelar.
+        const isConfirm = confirm("Você está CANCELANDO um Agendamento, ok?");
 
         if (isConfirm) {
-          console.log("REMOVER!");
+          // Faz a requisição na API para cancelar.
+          await scheduleCancel({ id });
+
+          // Recarrega o agendamento.
+          schedulesDay();
         }
       }
     }
