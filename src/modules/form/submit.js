681c25382fe8ab78479d355af0c5f82f39ab514a
Vnz-007 <viniciusloiolajg@gmail.com>
2025-03-09 09:27:34 -0300

    Recarrega            os agendamentos

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js";
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -41,11 +42,15 @@ form.onsubmit = async (event) => {
     // Gera um ID.
     const id = new Date().getTime();
 
+    // Faz o agendamento.
     await scheduleNew({
       id,
       name,
       when,
     });
+
+    // Recarrega os agendamentos.
+    await schedulesDay();
   } catch (error) {
     alert("Não foi possível realizar o agrndamento.");
     console.log(error);
