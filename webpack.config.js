78667883ccfc2d7ede7fbb97768531abd07c7ad7
Vnz-007 <viniciusloiolajg@gmail.com>
2025-03-09 14:26:58 -0300

    cancel-test

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ module.exports = {
     new htmlWebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
       favicon: path.resolve("src", "assets", "scissors.png"),
-      logo: path.resolve("src", "assets", "src/assets/Logo.png"),
     }),
     new copyWebpackPlugin({
       patterns: [
