907c8b6a397727b7595e9affea5108148ffb6b20
Vnz-007 <viniciusloiolajg@gmail.com>
2025-03-09 12:31:43 -0300

    Cancelamento do agendamento

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,3 +1,22 @@
 const periods = document.querySelectorAll(".period");
 
 // Gera evento click para cada lista (li = manhã, tarde e noite).
+periods.forEach((periods) => {
+  periods.addEventListener("click", (event) => {
+    if (event.target.classList.contains("cancel-icon")) {
+      // Obtém a li pai do elemento clicado.
+      const item = event.target.cosest("li");
+      const { id } = item.dataset;
+
+      if (id) {
+        const isConfirm = confirm(
+          "Você realmente deseja cancelar o agendamento?"
+        );
+
+        if (isConfirm) {
+          console.log("REMOVER!");
+        }
+      }
+    }
+  });
+});
